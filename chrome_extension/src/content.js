2a379cbaa0093d3c5748c207434be7881511093c
kinyusui <kaixngchen@gmail.com>
2025-02-07 09:06:26 -0500

    Incomplete public use. Instructions for how to run project.

--- chrome_extension/src/content.js
+++ chrome_extension/src/content.js
@@ -5,25 +5,25 @@ console.log("Content script injected!");
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "SEARCH") {
-    // const itemInfo = message.payload.replaceAll("\r", "");
-    const itemInfo = `Item Class: Rings
-Rarity: Rare
-Gale Hold
-Ruby Ring
---------
-Requirements:
-Level: 51
---------
-Item Level: 73
---------
-+21% to Fire Resistance (implicit)
---------
-Adds 3 to 44 Lightning damage to Attacks
-+208 to Accuracy Rating
-+93 to maximum Mana
-22% increased Rarity of Items found
-+17% to Fire Resistance
-+26% to Lightning Resistance`;
+    const itemInfo = message.payload.replaceAll("\r", "");
+    //     const itemInfo = `Item Class: Rings
+    // Rarity: Rare
+    // Gale Hold
+    // Ruby Ring
+    // --------
+    // Requirements:
+    // Level: 51
+    // --------
+    // Item Level: 73
+    // --------
+    // +21% to Fire Resistance (implicit)
+    // --------
+    // Adds 3 to 44 Lightning damage to Attacks
+    // +208 to Accuracy Rating
+    // +93 to maximum Mana
+    // 22% increased Rarity of Items found
+    // +17% to Fire Resistance
+    // +26% to Lightning Resistance`;
     console.log("Received message:", itemInfo);
     const piloter = new Piloter();
     piloter.setStatFilters(itemInfo);
---
+++ instructions.md
@@ -0,0 +1,1 @@
+Look for instructions.md in each project directory.
